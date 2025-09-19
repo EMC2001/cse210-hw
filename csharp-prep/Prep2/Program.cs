ccf16f98f40311c14047524e33a0cea67633db04
EMC2001 <emilianocastellanos0@gmail.com>
2025-09-19 15:34:06 -0600

    Prep 2 code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade percentage: ");
+        string grade = Console.ReadLine();
+        int numberGrade = int.Parse(grade);
+
+        string letterGrade = "";
+
+        if (numberGrade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (numberGrade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (numberGrade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (numberGrade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"You grade: {letterGrade}");
+
+        if (numberGrade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try harder bro");
+        }
     }
 }
