fd355fc08c76b2d3a3744cb41b7c9bbadf0332f7
EMC2001 <emilianocastellanos0@gmail.com>
2025-10-10 18:50:47 -0600

    Guessing Game Prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Clear();
+        Console.WriteLine("Welcome to the guessing game! Start guessing a number from 1 to 100");
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int guessNumber = -1;
+
+        while (guessNumber != number)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guessNumber = int.Parse(userGuess);
+        
+            if (guessNumber == number)
+            {
+                Console.Write("Right on!");
+            }
+            else if (guessNumber > number)
+            {
+                Console.WriteLine("Guess Lower");
+            }
+            else
+            {
+                Console.WriteLine("Guess Higher");
+            }
+        }
+      
     }
 }
