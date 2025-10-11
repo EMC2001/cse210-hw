c9826d7799e67c15fe1ceade04e3062ec8079b95
EMC2001 <emilianocastellanos0@gmail.com>
2025-10-10 20:44:47 -0600

    Prep 4 completed program.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.Clear();
+        Console.WriteLine("Enter numbers to add to the list. Press 0 to stop.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            string numberStr = Console.ReadLine();
+            number = int.Parse(numberStr);
+
+
+            if (number == 0)
+            {
+                double avg = numbers.Average();
+                int highest = numbers.Max();
+
+                Console.WriteLine($"The sum is {numbers.Sum()}");
+                Console.WriteLine($"The average is: {avg}");
+                Console.WriteLine($"The highest number is: {highest}");
+                break;
+            }
+
+            numbers.Add(number);
+        }
+
+
     }
 }
