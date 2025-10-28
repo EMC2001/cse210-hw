fb3a26cc5a7e43788d574dc00da3fda8d1c591da
EMC2001 <emilianocastellanos0@gmail.com>
2025-10-27 21:19:04 -0600

    Journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,69 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
-    }
+        List<Prompt> prompts = new List<Prompt>
+        {
+
+        new Prompt("Who was the most interesting person I interacted with today?"),
+        new Prompt("What was the best part of my day?"),
+        new Prompt("How did I see the hand of the Lord in my life today?"),
+        new Prompt("What was the strongest emotion I felt today?"),
+        new Prompt("If I had one thing I could do over today, what would it be?")
+        
+        };
+       
+       Journal journal = new Journal();
+       Random random = new Random();
+
+        while (true)
+        {
+            Menu.DisplayMenu();
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                int index = random.Next(prompts.Count);
+                string randomPrompt = prompts[index]._promptIdea;
+
+                Console.WriteLine($"{randomPrompt}");
+                Console.Write("Your response:");
+                string response = Console.ReadLine();
+
+                journal.addEntry(randomPrompt, response);
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("\nJournal Entries:");
+                journal.DisplayEntries();
+            }
+            else if (choice == "4")
+            {
+                Console.Write("Enter file name to save: ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+
+            else if (choice == "3")
+            {
+                Console.Write("Enter file name to load: ");
+                string filename = Console.ReadLine();
+                
+                Journal newJournal = new Journal();
+                newJournal.LoadFromFile(filename);
+
+                journal = newJournal;
+            }
+
+            else if (choice == "5")
+            {
+                Console.WriteLine("Exiting Program");
+                break;
+            }
+        }
+    }    
 }
